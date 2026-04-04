61f2621e690aeef1f85237551fca13290d05d5e9
Saidaminov Vosilhoja <vosilhojasaidaminov@gmail.com>
2026-04-04 15:41:43 +0500

    init

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,18 @@
+import "@/src/styles/globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export default async function LocaleLayout({children, params}: {children: React.ReactNode, params: {locale: string}}) {
+  const { locale } = await params
+  const messages = await getMessages()
+  
+  return (
+    <html lang={ locale }>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          { children }
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
